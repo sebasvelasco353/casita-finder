6fd2240c79c385f204a603c8c46f36c26a1f7211
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2026-08-15 12:54:47 -0500

    first commit
    first commit
    
    refactor: Basic project

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <span className="logo">Una Casita</span>
+      <nav>
+        <a href="/casitas">Casitas</a>
+        <a href="/bodegas">Bodegaje</a>
+      </nav>
+    </header>
+  )
+}
